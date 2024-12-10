e1bf5a21a93f24cf93254c04652946ba5a961ea4
Soutick <soutickadak99@gmail.com>
2024-12-10 09:40:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-API_ID = ""
-API_HASH = ""
-TOKEN = ""
+API_ID = 28450765
+API_HASH = "36f00f11f9d5c65e69b81fd804453a93"
+TOKEN = "7935129195:AAE93iKW370tuQwmYzEurq3pqBDVDC06cy4"
 
 app = Client("tagremover", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 
