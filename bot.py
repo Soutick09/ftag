2a0d708da752bdbc9eade6832ca1f47f6d108240
Soutick <soutickadak99@gmail.com>
2024-12-10 10:01:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,11 @@ TOKEN = "7935129195:AAE93iKW370tuQwmYzEurq3pqBDVDC06cy4"
 
 app = Client("tagremover", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 
+# Command handler for /start
+@app.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    await message.reply_text("Welcome to Tag Remover Bot!\n\nMade With ♥️ By Soutick")
+
 @app.on_message(filters.private & filters.text | filters.media)
 async def tag(client, message):
     await message.copy(message.chat.id)
