e59cb6813d4654fc4c7f31452981178c4c81b2a4
Soutick <soutickadak99@gmail.com>
2024-12-10 10:29:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 API_ID = 28450765
 API_HASH = "36f00f11f9d5c65e69b81fd804453a93"
-TOKEN = "7935129195:AAE93iKW370tuQwmYzEurq3pqBDVDC06cy4"
+TOKEN = "7935129195:AAFdhMvLXFwqtrxq2h2lXwnKvbCR1YJIBm8"
 
 app = Client("tagremover", bot_token=TOKEN, api_id=API_ID, api_hash=API_HASH)
 
